7c15d9717546fca08e16a946ce979b9d93da36eb
Batsura <Batsura@GRAYHP>
2017-02-26 22:44:15 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("     ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("   ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("  ");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("     ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("     ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("   ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("  ");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("     ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("   ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("   ");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("   ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("   ");
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("       ");
+
+            Console.ReadLine();
+                
+       }
+    }
+}
